1759fdf8f3223ba0db13067b109021813e36ff0b
Roberto <robertodermato@gmail.com>
2019-11-24 18:07:49 -0300

    segundo lambda

--- src/AppPessoa.java
+++ src/AppPessoa.java
@@ -13,26 +13,40 @@ public class AppPessoa {
 
         ArrayList<Pessoa> lista = new ArrayList<>();
 
-        Pessoa p1 = new Pessoa("Roberto", Pessoa.Genero.MASC, 43);
-        Pessoa p2 = new Pessoa ("Pedro", Pessoa.Genero.MASC, 20);
-        Pessoa p3 = new Pessoa ("Lucas", Pessoa.Genero.MASC, 19);
-        Pessoa p4 = new Pessoa ("Bernardo", Pessoa.Genero.MASC, 3);
-        Pessoa p5 = new Pessoa ("Adriana", Pessoa.Genero.FEM, 18);
+        Pessoa p1 = new Pessoa("Roberto", Pessoa.Genero.MASC, 43, "roberto@gmail.com");
+        Pessoa p2 = new Pessoa ("Pedro", Pessoa.Genero.MASC, 20, "pedro@gmail.com");
+        Pessoa p3 = new Pessoa ("Lucas", Pessoa.Genero.MASC, 19, "lucas@gmail.com");
+        Pessoa p4 = new Pessoa ("Bernardo", Pessoa.Genero.MASC, 3, "bernardo@gmail.com");
+        Pessoa p5 = new Pessoa ("Adriana", Pessoa.Genero.FEM, 18, "adriana@gmail.com");
+        Pessoa p6 = new Pessoa ("Ana Maria", Pessoa.Genero.FEM, 73, "ana@gmail.com");
+        Pessoa p7 = new Pessoa ("Gladir", Pessoa.Genero.FEM, 72, "gladir@gmail.com");
 
         lista.add(p1);
         lista.add(p2);
         lista.add(p3);
         lista.add(p4);
         lista.add(p5);
+        lista.add(p6);
+        lista.add(p7);
 
         System.out.println("Todas as pessoas: " + lista);
 
-        System.out.println("Pessoas com mais de 18 anos");
         //Exibir o nome das pessoas maiores de 18 anos
+        System.out.println("\nPessoas com mais de 18 anos");
         lista
                 .stream()
                 .filter(p -> p.getIdade() > 18)
                 .forEach(p -> System.out.println(p));
+
+        //Exibir o email das mulheres maiores de 60 anos
+        System.out.println("\nemail das mulheres com mais de 60 anos");
+        lista
+                .stream()
+                .filter(p -> p.getIdade() > 60)
+                .filter(p -> p.getGenero()== Pessoa.Genero.FEM)
+                .forEach(p-> System.out.println("Email: " + p.getEmail()));
+
+//        Exibir a média de idade dos homens
 
 
 
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -7,15 +7,19 @@ public class Pessoa {
     String nome;
     Genero genero;
     int idade;
+    String email;
 
-    public Pessoa(String nome, Genero genero, int idade) {
+    public Pessoa(String nome, Genero genero, int idade, String email) {
         this.nome = nome;
         this.genero = genero;
         this.idade = idade;
+        this.email = email;
             }
 
     public int getIdade() { return idade; }
     public String getNome() { return nome; }
+    public String getEmail() {return email;}
+    public Genero getGenero() {return genero;}
 
     @Override
     public String toString() {
